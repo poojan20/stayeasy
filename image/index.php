f1c7c41eddbc1baaaecfb7f6f496a063ee4a8a29
pranavvadhel <vadhelpranav4991@gmail.com>
2024-01-13 18:47:41 +0530

    Change index[5] logo [changed spacing],[3]

--- image/index.php
+++ image/index.php
@@ -26,14 +26,14 @@
     <!--Navbar from bootstrap5-->
     <nav class="navbar navbar-expand-lg navbar-light px-lg-3 py-lg-2 shadow-sm sticky-top bg-body-tertiary ">
     <div class="container-fluid">
-        <a class="navbar-brand me-3" href="index.php"><font size = 5><strong>Stay Easy</strong></font></a>
+        <a class="navbar-brand me-3" href="index.php"><font size = 5><strong>StayEasy</strong></font></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-            <a class="nav-link active me-2" aria-current="page" href="#">Home</a>
+            <a class="nav-link  me-2" aria-current="page" href="#">Home</a>
             </li>
             <li class="nav-item">
             <a class="nav-link me-2" href="#">Rooms</a>
