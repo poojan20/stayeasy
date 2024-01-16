9a6dd96c5d0b1a41b9fc9b7035508f78f39c74f9
Poojan442 <trivedipoojan92@gmail.com>
2024-01-16 12:56:12 +0530

    Change index[9] Changed index.php[Added forms],[5]

--- image/index.php
+++ image/index.php
@@ -20,6 +20,24 @@
     .h-font{
         font-family: 'Merienda', cursive;
     }
+    .custom-bg{
+        background-color:#0D1117;
+    }
+    .custom-bg:hover{
+        background-color:#111e2e;
+    }
+    .availability-form{
+        margin-top: -50px;
+        z-index: 2;
+        position: relative;
+    }
+    @media screen and (max-width:575px){
+        .availability-form{
+        margin-top: 25px;
+        padding: 0 35px;    
+        }
+    } 
+    
    
 
 </style>
@@ -226,24 +244,41 @@
 
 <!-- Check Availability Form -->
 
-<div class="container">
+<div class="container availability-form">
     <div class="row">
         <div class="col-lg-12 bg-white shadow p-4 rounded">
-            <h5>Check Booking Availability</h5>
+            <h5 class="mb-4">Check Booking Availability</h5>
             <form>
-                <div class="row">
-                    <div class = col-lg-3>
+                <div class="row align-items-end">
+                    <div class = "col-lg-3 mb-3">
                         <label  class="form-label" style ="font-width: 500;">Check-in</label>
                         <input type="date" class="form-control shadow-none">
                     </div>
-                    <div class = col-lg-3>
+                    <div class = "col-lg-3 mb-3">
                         <label  class="form-label" style ="font-width: 500;">Check-out</label>
                         <input type="date" class="form-control shadow-none">
                     </div>
-                    <div class="col-lg-3">
-                    <label  class="form-label" style ="font-width: 500;">Adult</label>
-                    
+                    <div class="col-lg-3 mb-3">
+                        <label  class="form-label" style ="font-width: 500;">Adult</label>
+                        <select class="form-select shadow-none">
+                            <option selected>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>   
                     </div>
+                    <div class="col-lg-2 mb-3">
+                        <label  class="form-label" style ="font-width: 500;">Children</label>
+                        <select class="form-select shadow-none">
+                            <option selected>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                        <div class="col-lg-1 mb-lg-3 mt-2">
+                            <button type = "submit" class= "btn text-white shadow-none custom-bg">Submit</button>
+                        </div>
                 </div>
             </form>
         </div>
