cc880d9e3a79535cbf499a3fe2fee3b8a4811d21
Poojan442 <trivedipoojan92@gmail.com>
2024-01-16 12:20:48 +0530

    Change index [9],changed index.php[added images and set swipers],[4]

--- image/index.php
+++ image/index.php
@@ -188,31 +188,68 @@
 </div>
 
 
-<div class="container-fluid"> 
+<!--Carousel-->
+
+<div class="container-fluid mt-4"> 
     <!-- Swiper -->
   <div class="swiper swiper-container">
     <div class="swiper-wrapper">
     <div class="swiper-slide">
-        <img src="/img/Stayeasy/stayeasy/image/carousel/1.jpeg" alt="Description of the image" />
+        <img src="/img/Stayeasy/stayeasy/image/carousel/1.jpeg" class ="w-100 d-block" />
     </div>
 
       <div class="swiper-slide">
-        <img src="/img/Stayeasy/stayeasy/image/carousel/2.jpeg" />
+        <img src="/img/Stayeasy/stayeasy/image/carousel/2.jpeg" class ="w-100 d-block" />
       </div>
       <div class="swiper-slide">
-        <img src="/img/Stayeasy/stayeasy/image/carousel/3.jpeg" />
+        <img src="/img/Stayeasy/stayeasy/image/carousel/3.jpeg" class ="w-100 d-block" />
       </div>
       <div class="swiper-slide">
-        <img src="/img/Stayeasy/stayeasy/image/carousel/4.jpeg" />
+        <img src="/img/Stayeasy/stayeasy/image/carousel/4.png" class ="w-100 d-block" />
       </div>
       <div class="swiper-slide">
-        <img src="/img/Stayeasy/stayeasy/image/carousel/5.jpeg" alt="Description of the image" />
+        <img src="/img/Stayeasy/stayeasy/image/carousel/5.jpeg"  class ="w-100 d-block"/>
     </div>
+    <div class="swiper-slide">
+        <img src="/img/Stayeasy/stayeasy/image/carousel/6.png" class ="w-100 d-block" />
+      </div>
+      <div class="swiper-slide">
+        <img src="/img/Stayeasy/stayeasy/image/carousel/7.png" class ="w-100 d-block" />
+      </div>
+      <div class="swiper-slide">
+        <img src="/img/Stayeasy/stayeasy/image/carousel/8.png" class ="w-100 d-block" />
+      </div>
     </div>
     
   </div>
 </div>
 
+<!-- Check Availability Form -->
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 bg-white shadow p-4 rounded">
+            <h5>Check Booking Availability</h5>
+            <form>
+                <div class="row">
+                    <div class = col-lg-3>
+                        <label  class="form-label" style ="font-width: 500;">Check-in</label>
+                        <input type="date" class="form-control shadow-none">
+                    </div>
+                    <div class = col-lg-3>
+                        <label  class="form-label" style ="font-width: 500;">Check-out</label>
+                        <input type="date" class="form-control shadow-none">
+                    </div>
+                    <div class="col-lg-3">
+                    <label  class="form-label" style ="font-width: 500;">Adult</label>
+                    
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<br><br><br><br><br><br>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
@@ -222,6 +259,11 @@
     var swiper = new Swiper(".swiper-container", {
       spaceBetween: 30,
       effect: "fade",
+      loop: true,
+      autoplay: {
+       delay: 3500, 
+       disableOnInteractoin: false,
+      }
     });
   </script>
 
