0e3e2af76bdbd73d0353001bc232b6b49c8ad6d0
Poojan442 <trivedipoojan92@gmail.com>
2024-01-19 13:22:57 +0530

    Change index [11],changed index.php,[added logo],[8]

--- image/index.php
+++ image/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="shortcut icon" href="s.png">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Stay easy</title>
