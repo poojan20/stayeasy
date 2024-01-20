6857988e8ef032fe1d18d8a3e8ee6193cdcbed51
Poojan442 <trivedipoojan92@gmail.com>
2024-01-20 17:45:52 +0530

    Change index[13], Changed index.php.[added new sections],[10]

--- image/index.php
+++ image/index.php
@@ -15,6 +15,24 @@
 <link rel="stylesheet" href="\img\stayeasy\stayeasy\css\common.css">
 
 <style>
+    ::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #0D1117; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
    
     .availability-form{
         margin-top: -50px;
@@ -24,7 +42,7 @@
     .btn{
         color: black;
         background-color: #FFFFFF;
-        border :  1px solid black;
+        border :  2px solid black;
     }
     .btn:hover{
         background-color: #111e2e;
@@ -506,7 +524,7 @@
         </div>
     </div>
 </div>
-
+<!-- Facilities -->
 <h2 class = "mt-5 pt-5 mb-4 text-center " ><font face = "Raleway" color = "white" size = 50>Facilitiess</font></h2>
 
     <div class="container">
@@ -532,28 +550,197 @@
                <h5 class = "mt-3">Wifi</h5> 
             </div>
             <div class="col-lg-12 text-center mt-5">
-                <a href="#" class="btn2 btn2-sm b rounded-2  text-decoration-none">More facilities >>></a>
+                <a href="#" class="btn2 btn2-sm  rounded-2  text-decoration-none">More facilities >>></a>
             </div>
         </div>
     </div>
 
-<br><br><br><br><br><br>
+<!-- Testimonials -->
+
+    <h2 class = "mt-5 pt-5 mb-4 text-center " ><font face = "Raleway" color = "white" size = 50>Testimonials</font></h2>
+
+    <div class="container">
+         <!-- Swiper -->
+  <div class="swiper swiper-testimonials">
+    <div class="swiper-wrapper mb-5">
+      <div class="swiper-slide bg-white p-4">
+        <div class="profile d-flex align-items-center mb-3">
+            <img src="\img\Stayeasy\stayeasy\image\features\wifi.svg" width="30px">
+            <h6 class = "m-0 ms-2">Random user 1</h6>
+        </div>
+        <p>Poojan pranav</p>
+        <div class="rating">
+        <h6 class ="mb-1">Rating</h6>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+        </div>
+    </div>
+    <div class="swiper-slide bg-white p-4">
+        <div class="profile d-flex align-items-center mb-3">
+            <img src="\img\Stayeasy\stayeasy\image\features\wifi.svg" width="30px">
+            <h6 class = "m-0 ms-2">Random user 2</h6>
+        </div>
+        <p>Poojan pranav</p>
+        <div class="rating">
+        <h6 class ="mb-1">Rating</h6>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+        </div>
+    </div>
+    <div class="swiper-slide bg-white p-4">
+        <div class="profile d-flex align-items-center mb-3">
+            <img src="\img\Stayeasy\stayeasy\image\features\wifi.svg" width="30px">
+            <h6 class = "m-0 ms-2">Random user 3</h6>
+        </div>
+        <p>Poojan pranav</p>
+        <div class="rating">
+        <h6 class ="mb-1">Rating</h6>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+                <i class="bi bi-star-fill text-warning"></i>
+        </div>
+    </div>
+    
+    <div class="swiper-pagination"></div>
+</div>
+        <div class="col-lg-12 text-center mt-5">
+            <a href="#" class="btn2 btn2-sm b rounded-2  text-decoration-none">Know More >>></a>
+        </div>
+    
+    </div>
+
+    <!-- Reach us -->
+
+    <h2 class = "mt-5 pt-5 mb-4 text-center " ><font face = "Raleway" color = "white" size = 50>Reach Us</font></h2>
+        
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-8 p-4 mb-lg-0 mb-3 bg-white rounded ">
+                <iframe class="w-100 rounded " height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d59286.78879159111!2d72.15490609999999!3d21.763833650000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395f5081abb84e2f%3A0xf676d64c6e13716c!2sBhavnagar%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1705747537362!5m2!1sen!2sin"  loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+            <div class="col-lg-4 col-md-4">
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Call us</h5>
+                    <a href="tel :+919586138313" class ="d-inline-block  text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i>+919586138313
+                    </a>
+                    <br>
+                    <a href="tel :+919586138313" class ="d-inline-block  text-decoration-none text-dark">
+                        <i class="bi bi-telephone-fill"></i>+919486138313
+                    </a>
+                </div>
+        
+                <div class="bg-white p-4 rounded mb-4">
+                    <h5>Follow us</h5>
+                    <a href="#" class ="d-inline-block mb-3 ">
+                        <span class ="badge bg-light text-dark fs-6 p-2">
+                        <i class="bi bi-twitter-x me-1"></i>Twitter-X
+                        </span>
+                    </a><br>
+                     <a href="#" class ="d-inline-block mb-3">
+                        <span class ="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-instagram me-1"></i>Instagram
+                        </span>
+                    </a><br>
+                    <a href="#" class ="d-inline-block">
+                        <span class ="badge bg-light text-dark fs-6 p-2">
+                            <i class="bi bi-facebook me-1"></i>Facebook
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="container-fluid bg-white mt-5">
+        <div class = "row">
+            <div class = "col-lg-4 p-4">
+                <h3 class="h2-font fw-bold fs-3 mb-2">StayEasy</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia aliquam 
+                    accusamus porro illum! Sequi alias inventore deserunt ut nihil quidem id 
+                    natus magni fuga quo, assumenda atque omnis enim adipisci.
+                </p>
+            </div>
+            <div class = "col-lg-4 p-4">
+                <h5 class = "mb-3">Links</h5>
+                <a href="#" class ="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+                <a href="#" class ="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+                <a href="#" class ="d-inline-block mb-2 text-dark text-decoration-none">Facilities<br>
+                <a href="#" class ="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a><br>
+                <a href="#" class ="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+            </div>
+            <div class = "col-lg-4 p-4">
+                <h5 class="mb-3">Follow us</h5>
+                <a href="#" class ="d-inline-block text-dark text-decoration-none mb-2 "><i class="bi bi-twitter-x me-1"></i>Twitter-X</a><br>
+                <a href="#" class ="d-inline-block text-dark text-decoration-none mb-2 "><i class="bi bi-facebook me-1"></i>Facebook</a><br>
+                <a href="#" class ="d-inline-block text-dark text-decoration-none  "><i class="bi bi-instagram me-1"></i>Instagram</a><br>
+            </div>
+        </div>
+</div>
+               
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
 
   <!-- Initialize Swiper -->
   <script>
-    var swiper = new Swiper(".swiper-container", {
-      spaceBetween: 30,
-      effect: "fade",
-      loop: true,
-      autoplay: {
-       delay: 3500, 
-       disableOnInteractoin: false,
-      }
-    });
-  </script>
+            var swiper = new Swiper(".swiper-container", {
+            spaceBetween: 30,
+            effect: "fade",
+            loop: true,
+            autoplay: {
+            delay: 3500, 
+            disableOnInteractoin: false,
+            }
+            });
+
+            var swiper = new Swiper(".swiper-testimonials", {
+            effect: "coverflow",
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: "auto",
+            slidesPerView: "3",
+            loop :true,
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+            },
+            breakpoints: {
+                320:{
+                    slidesPerView: 1,
+                },
+                640:{
+                    slidesPerView: 1,
+                },
+                768:{
+                    slidesPerView: 2,
+                },
+                1024:{
+                    slidesPerView: 3,
+                },
+            }
+            });
+    </script>
+
+    
+
+
 
 </body>
 </html>
