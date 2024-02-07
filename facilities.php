22b278224e36e622cfe46e4de5077d5231279781
Poojan442 <trivedipoojan92@gmail.com>
2024-02-07 10:20:45 +0530

    error fix

--- facilities.php
+++ facilities.php
@@ -14,24 +14,23 @@
                 transition : 0.3s;
         }
         
-::-webkit-scrollbar {
-    width: 10px;
-  }
-  
-  /* Track */
-  ::-webkit-scrollbar-track {
-    background: #0D1117; 
-  }
-   
-  /* Handle */
-  ::-webkit-scrollbar-thumb {
-    background: #888; 
-  }
-  
-  /* Handle on hover */
-  ::-webkit-scrollbar-thumb:hover {
-    background: #555; 
-  }
+        ::-webkit-scrollbar {
+                    width: 10px;
+                }
+                
+                /* Track */
+        ::-webkit-scrollbar-track {
+                    background: #0D1117; 
+                }
+                /* Handle */
+        ::-webkit-scrollbar-thumb {
+                    background: #888; 
+                }
+                
+                /* Handle on hover */
+        ::-webkit-scrollbar-thumb:hover {
+                    background: #555; 
+                }
      
         
    </style>
